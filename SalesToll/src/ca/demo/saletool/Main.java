30794af3bb0b0c75a9efeb3f2846bab2418914d9
azam977 <azam497@yahoo.com>
2014-12-30 21:41:36 +0000

    Initial checking of sales tool.

---
+++ SalesToll/src/ca/demo/saletool/Main.java
@@ -0,0 +1,19 @@
+package ca.demo.saletool;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		displayGreeting();
+		data.display();
+		
+
+	}
+private static void displayGreeting()
+
+{
+	System.out.println("Hello Happy Sales");
+	System.out.println("this app shows sales");
+	
+}
+}
